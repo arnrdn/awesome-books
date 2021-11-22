ae114275d84799f5821840905b61a895c328bdc1
rdnrn <rodina.arina00@gmail.com>
2021-11-22 16:41:40 +0300

    Add dynamic book card

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-const bookAuthor = document.getElementById('author');
-const bookTitle = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const titleInput = document.getElementById('title');
 const addBtn = document.getElementById('add');
+const bookList = document.getElementsByClassName('book-cards');
+const id = null;
 
 const bookArr = [];
 
-const book = {
-  title: 'Book title',
-  author: 'Book author'
-};
-
-
-const newBook = function () {
-  const anotherBook = Object.create(book);
-  this.title = ;
-  this.author = ;
-
-  bookArr.push();
+function display() {
+  bookList.innerHTML = '';
+  bookArr.forEach((element) => {
+    const BookCard = document.createElement('li');
+    const BookCardTwo = document.createElement('li');
+    const RemoveBtn = document.createElement('button');
+    RemoveBtn.classList = 'remove_btn';
+    RemoveBtn.setAttribute('id', element.id);
+    RemoveBtn.setAttribute('onclick', `${'remoteAt(this.id)'}`);
+    BookCard.innerHTML = element.booksName;
+    BookCardTwo.innerHTML = element.booksAu;
+    RemoveBtn.innerHTML = 'Remove';
+    bookList.appendChild(BookCard);
+    bookList.appendChild(BookCardTwo);
+    bookList.appendChild(RemoveBtn);
+    bookList.appendChild(bookList);
+    titleInput.value = '';
+    authorInput.value = '';
+  });
+  localStorage.clear();
+  localStorage.setItem('bookArr', JSON.stringify(bookArr));
 }
-
