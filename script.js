fe4cc303f029c7e22babd9076b11c7cbad5eae01
rdnrn <rodina.arina00@gmail.com>
2021-11-22 16:06:22 +0300

    Change the html markup start script js

---
+++ script.js
@@ -0,0 +1,20 @@
+const bookAuthor = document.getElementById('author');
+const bookTitle = document.getElementById('title');
+const addBtn = document.getElementById('add');
+
+const bookArr = [];
+
+const book = {
+  title: 'Book title',
+  author: 'Book author'
+};
+
+
+const newBook = function () {
+  const anotherBook = Object.create(book);
+  this.title = ;
+  this.author = ;
+
+  bookArr.push();
+}
+
