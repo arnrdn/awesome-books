83e247ad87163e50b2d8369ab1a4d197a2753817
direakanbi <83509786+direakanbi@users.noreply.github.com>
2021-11-23 13:49:34 +0100

    Refactored Objects to Use classes

--- script.js
+++ script.js
@@ -1,86 +1,70 @@
-/* eslint-disable max-classes-per-file */
-/* eslint-disable no-unused-vars */
-
 const id = null;
 const bookInput = document.querySelector('#txt');
 const bookInputOne = document.querySelector('#txtOne');
 const addBtn = document.querySelector('.add');
 const bookList = document.querySelector('.book-list');
+let bookArray = JSON.parse(localStorage.getItem('bookArray'));
 
 class Books {
   constructor(id, booksName, booksAu) {
+    if (bookArray == null) {
+      bookArray = [];
+    }
     this.booksName = booksName;
     this.booksAu = booksAu;
     this.id = id;
-  }
-}
 
-class BookStore {
-  static getBooks() {
-    let bookArray;
-    if (localStorage.getItem('bookArray') === null) {
-      bookArray = [];
-    } else {
-      bookArray = JSON.parse(localStorage.getItem('bookArray'));
-    }
-    return bookArray;
-  }
-
-  static addButton() {
-    const bookArray = BookStore.getBooks();
     addBtn.addEventListener('click', (e) => {
       if (bookInput.value !== '') {
         e.preventDefault();
-        const object = new Books(
-          Math.random().toString(16).slice(2),
-          bookInput.value,
-          bookInputOne.value,
-        );
+        const object = 
+        new Books ( 
+           Math.random().toString(16).slice(2),
+           bookInput.value,
+           bookInputOne.value,
+         )
+        
         bookArray.push(object);
-        BookStore.display();
+        Books.display();
       }
-      localStorage.setItem('bookArray', JSON.stringify(bookArray));
     });
-  }
+ }
 
-  static removeAt(id) {
-    const bookArray = BookStore.getBooks();
-    const element = document.getElementById(id);
-    const index = bookArray.findIndex((prop) => prop.id === id);
-    bookArray.splice(index, 1);
-    element.parentElement.style.display = 'none';
-    BookStore.display();
-    localStorage.setItem('bookArray', JSON.stringify(bookArray));
-  }
+ static display() {
+  bookList.innerHTML = '';
+  bookArray.forEach((element) => {
+    const MyBooks = document.createElement('ul');
+    const MyList = document.createElement('li');
+    const MyListTwo = document.createElement('li');
+    const Mybutton = document.createElement('button');
+    Mybutton.classList = 'remove_btn';
+    Mybutton.setAttribute('id', element.id);
+    Mybutton.setAttribute('onclick', `${'Books.remoteAt(this.id)'}`);
+    MyList.innerHTML = element.booksName;
+    MyListTwo.innerHTML = element.booksAu;
+    Mybutton.innerHTML = 'Remove';
+    MyBooks.appendChild(MyList);
+    MyBooks.appendChild(MyListTwo);
+    MyBooks.appendChild(Mybutton);
+    bookList.appendChild(MyBooks);
+    bookInput.value = '';
+    bookInputOne.value = '';
+  });
+  localStorage.clear();
+  localStorage.setItem('bookArray', JSON.stringify(bookArray));
+} 
+
+static remoteAt(id) {
+  const element = document.getElementById(id);
+  const index = bookArray.findIndex((prop) => prop.id === id);
+  bookArray.splice(index, 1);
+  element.parentElement.style.display = 'none';
+  display();
+}
 }
 
-class BookDisplay {
-  static display() {
-    const bookArray = BookStore.getBooks();
-    bookList.innerHTML = '';
-    bookArray.forEach((element) => {
-      const MyBooks = document.createElement('ul');
-      const MyList = document.createElement('li');
-      const MyListTwo = document.createElement('li');
-      const Mybutton = document.createElement('button');
-      Mybutton.classList = 'remove_btn';
-      Mybutton.setAttribute('id', element.id);
-      Mybutton.setAttribute('onclick', `${'Books.removeAt(this.id)'}`);
-      MyList.innerHTML = element.booksName;
-      MyListTwo.innerHTML = element.booksAu;
-      Mybutton.innerHTML = 'Remove';
-      MyBooks.appendChild(MyList);
-      MyBooks.appendChild(MyListTwo);
-      MyBooks.appendChild(Mybutton);
-      bookList.appendChild(MyBooks);
-      bookInput.value = '';
-      bookInputOne.value = '';
-    });
-    localStorage.clear();
-    localStorage.setItem('bookArray', JSON.stringify(bookArray));
-  }
-}
-
-// document.addEventListener('DOMContentLoaded', BookStore.display);
-
-// addBtn.addEventListener('submit', BookStore.addButton);
+window.addEventListener('load', () => {
+  Books.display();
+  Books.remoteAt(id);
+  Books.addBtn();
+});
