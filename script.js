d6f2e59bb8e248224a7bc232838d6ee7367c2ed7
direakanbi <83509786+direakanbi@users.noreply.github.com>
2021-11-23 14:23:42 +0100

    Refactored JavaScript to use Classes Instead of objects

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const bookInputOne = document.querySelector('#txtOne');
 const addBtn = document.querySelector('.add');
 const bookList = document.querySelector('.book-list');
 let bookArray = JSON.parse(localStorage.getItem('bookArray'));
-
 class Books {
   constructor(id, booksName, booksAu) {
     if (bookArray == null) {
@@ -13,58 +12,53 @@ class Books {
     this.booksName = booksName;
     this.booksAu = booksAu;
     this.id = id;
+  }
 
-    addBtn.addEventListener('click', (e) => {
-      if (bookInput.value !== '') {
-        e.preventDefault();
-        const object = 
-        new Books ( 
-           Math.random().toString(16).slice(2),
-           bookInput.value,
-           bookInputOne.value,
-         )
-        
-        bookArray.push(object);
-        Books.display();
-      }
+  static display() {
+    bookList.innerHTML = '';
+    bookArray.forEach((element) => {
+      const MyBooks = document.createElement('ul');
+      const MyList = document.createElement('li');
+      const MyListTwo = document.createElement('li');
+      const Mybutton = document.createElement('button');
+      Mybutton.classList = 'remove_btn';
+      Mybutton.setAttribute('id', element.id);
+      Mybutton.setAttribute('onclick', `${'Books.remoteAt(this.id)'}`);
+      MyList.innerHTML = element.booksName;
+      MyListTwo.innerHTML = element.booksAu;
+      Mybutton.innerHTML = 'Remove';
+      MyBooks.appendChild(MyList);
+      MyBooks.appendChild(MyListTwo);
+      MyBooks.appendChild(Mybutton);
+      bookList.appendChild(MyBooks);
+      bookInput.value = '';
+      bookInputOne.value = '';
     });
- }
+    localStorage.clear();
+    localStorage.setItem('bookArray', JSON.stringify(bookArray));
+  }
 
- static display() {
-  bookList.innerHTML = '';
-  bookArray.forEach((element) => {
-    const MyBooks = document.createElement('ul');
-    const MyList = document.createElement('li');
-    const MyListTwo = document.createElement('li');
-    const Mybutton = document.createElement('button');
-    Mybutton.classList = 'remove_btn';
-    Mybutton.setAttribute('id', element.id);
-    Mybutton.setAttribute('onclick', `${'Books.remoteAt(this.id)'}`);
-    MyList.innerHTML = element.booksName;
-    MyListTwo.innerHTML = element.booksAu;
-    Mybutton.innerHTML = 'Remove';
-    MyBooks.appendChild(MyList);
-    MyBooks.appendChild(MyListTwo);
-    MyBooks.appendChild(Mybutton);
-    bookList.appendChild(MyBooks);
-    bookInput.value = '';
-    bookInputOne.value = '';
-  });
-  localStorage.clear();
-  localStorage.setItem('bookArray', JSON.stringify(bookArray));
-} 
-
+  
 static remoteAt(id) {
   const element = document.getElementById(id);
   const index = bookArray.findIndex((prop) => prop.id === id);
   bookArray.splice(index, 1);
   element.parentElement.style.display = 'none';
-  display();
+  Books.display();
 }
 }
+
+addBtn.addEventListener('click', (e) => {
+  if (bookInput.value !== '') {
+    e.preventDefault();
+    const object = new Books(Math.random().toString(16).slice(2),
+      bookInput.value, bookInputOne.value);
+    bookArray.push(object);
+    Books.display();
+  }
+});
 
 window.addEventListener('load', () => {
   Books.display();
   Books.remoteAt(id);
-  Books.addBtn();
 });
